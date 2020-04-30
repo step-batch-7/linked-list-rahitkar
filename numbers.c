220fd26f9b6de5c468397884a9e7826883f9d43c
Rahit <rahitkar@users.noreply.github.com>
2020-04-30 20:55:21 +0530

    changed char* to Char_ptr in show_result function

--- numbers.c
+++ numbers.c
@@ -6,7 +6,7 @@ void show_menu()
   printf("Main Menu\n---------\n(a) add a number to the end of the list\n(b) add a number to the start of the list\n(c) insert a number at a given position in the list\n(d) add a unique item on the list at the end(if it alreay exists, do not insert)\n(e) remove a number from the beginning of the list\n(f) remove a number from the end of the list\n(g) remove a number from a given position in the list\n(h) remove first occurrence of a number\n(i) remove all occurrences of a number\n(j) clear the whole list\n(k) check if a number exists in the list\n(l) display the list of numbers\n(m) exit\n\nPlease enter the alphabet of the operation you would like to perform ");
 }
 
-void show_result(Status result, char* message)
+void show_result(Status result, Char_ptr message)
 {
   if (result)
   {
