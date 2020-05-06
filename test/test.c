d14be3dd19b39973754ca12dcd1006101f0aab07
Rahit <rahitkar@users.noreply.github.com>
2020-05-06 13:52:54 +0530

    added assert tests for add_to_start

--- test/test.c
+++ test/test.c
@@ -4,7 +4,7 @@
 #include "../list.h"
 
 //________assert_functions__________
-void assert_null_equal(void* expectedValue, void* actualValue, Char_ptr message)
+void assert_null_equal(void *expectedValue, void *actualValue, Char_ptr message)
 {
   printf("%s\n", message);
   if (actualValue != expectedValue)
@@ -46,11 +46,29 @@ void test_create_node()
   printf("\n");
 }
 
+void test_add_to_start()
+{
+  printf("test_add_to_start\n");
+  List_ptr list = create_list();
+  assert_int_equal(Success, add_to_start(list, 4), "should return Success status after adding 4 in a empty list");
+  assert_int_equal(1, list->count, "count should incrise by 1");
+  assert_int_equal(4, list->head->value, "list's head's value should be 4 for adding it in a empty list");
+  assert_int_equal(4, list->last->value, "list's last's value should be 4 for adding it in a empty list");
+  assert_null_equal(NULL, list->last->next, "list's last's next should be null");
+
+  assert_int_equal(Success, add_to_start(list, 6), "should return Success status after adding 6 in the first position of the given list");
+  assert_int_equal(2, list->count, "count should incrise by 1");
+  assert_int_equal(6, list->head->value, "list's head's value should be 6 for adding it in the given list");
+  assert_int_equal(4, list->last->value, "list's last's value should remain 4 after adding 6 in the first position of given list");
+  printf("\n");
+}
+
 // _________runTest_________________
 void runTest(void)
 {
   test_create_list();
   test_create_node();
+  test_add_to_start();
 }
 
 int main(void)
